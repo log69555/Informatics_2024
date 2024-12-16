814975037488a0254ec2136371c9df18ab5889b1
log69555 <143123261+log69555@users.noreply.github.com>
2024-12-16 16:57:39 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -1,8 +1,9 @@
 package main
 
+package main
+
 import (
 	"fmt"
-
 
 	"isuct.ru/informatics2022/lab"
 )
@@ -12,25 +13,4 @@ func main() {
 	fmt.Println("\n8 лабороторная")
 
 	lab.Show_lab8()
-
-	"isuct.ru/informatics2022/lab4"
-	"isuct.ru/informatics2022/lab6"
-	"isuct.ru/informatics2022/lab7"
-)
-
-func main() {
-	fmt.Println("Шхов Егор")
-
-	fmt.Println("\n4 лабороторная")
-
-	lab4.Show_lab4()
-
-	fmt.Println("\n6 лабороторная")
-
-	lab6.Show_lab6()
-
-	fmt.Println("\n7 лабороторная")
-
-	lab7.Show_lab7()
-
 }
