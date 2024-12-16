1970a4bb22699fcd64e966ff09064a653496533a
log69555 <143123261+log69555@users.noreply.github.com>
2024-12-16 19:16:49 +0300

    8 lab_fix3

--- golang/main.go
+++ golang/main.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 
 	"isuct.ru/informatics2022/lab"
+	"isuct.ru/informatics2022/lab4"
+	"isuct.ru/informatics2022/lab6"
+	"isuct.ru/informatics2022/lab7"
 )
 
 func main() {
 	fmt.Println("Шахов Егор")
-	fmt.Println("\n8 лабороторная")
 
+	fmt.Println("\n4 лабороторная")
+	lab4.Show_lab4()
+
+	fmt.Println("\n6 лабороторная")
+	lab6.Show_lab6()
+
+	fmt.Println("\n7 лабороторная")
+	lab7.Show_lab7()
+
+	fmt.Println("\n8 лабороторная")
 	lab.Show_lab8()
 }
